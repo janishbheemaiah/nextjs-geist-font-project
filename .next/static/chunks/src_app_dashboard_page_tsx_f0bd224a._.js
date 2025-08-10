d623d67f4d7e520480e67bc080890a14dc197851
Janish Bheemaiah <janishbheemaiah304@gmail.com>
2025-08-10 15:58:28 +0530

    Add .next/static/chunks/src_app_dashboard_page_tsx_f0bd224a._.js 1754821692770

---
+++ .next/static/chunks/src_app_dashboard_page_tsx_f0bd224a._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_7c061bc2._.js",
+  "static/chunks/node_modules_ef597255._.js"
+],
+    source: "dynamic"
+});
